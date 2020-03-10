bf93a27ec94e147f8c766144c18656b63c29c910
Denis Sheikherev <denis.sheikherev@gmail.com>
2020-03-10 22:14:26 +0700

    feat: adds day27/Project5

---
+++ day_27/Project5/Project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Denis Sheikherev on 10.03.2020.
+//  Copyright © 2020 Denis Sheikherev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
